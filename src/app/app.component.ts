1edc7e48deb86795b8f183aa5a36d39a69169c50
FloralbXhafa <xhafafloralb@gmail.com>
2022-01-17 20:00:03 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CounterApp';
+
+  countNumber = 0;
+
+  resetBtn(){
+    this.countNumber = 0;
+  }
+
+  increaseBtn(){
+    this.countNumber = this.countNumber + 1;
+  }
+
+  decreaseBtn(){
+    this.countNumber = this.countNumber - 1;
+    if(this.countNumber<0)
+    {
+      this.countNumber=0;
+    }
+  }
 }
